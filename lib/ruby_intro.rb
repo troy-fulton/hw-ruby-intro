4d6e6e136d4e675d7a7f20a9c1720ebc7244d915
EC2 Default User <ec2-user@ip-172-31-93-35.ec2.internal>
2020-01-26 23:10:43 +0000

    Part 1,2,3 Done in one swoop

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,68 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  for elem in arr
+    sum += elem
+  end
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  end
+  if arr.length < 2
+    return arr[0]
+  end
+  arr = arr.sort.reverse
+  return arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length <= 1
+    return false
+  end
+  tmp_arr = arr
+  for test_op1 in arr
+    tmp_arr = tmp_arr.drop(1)
+    for test_op2 in tmp_arr
+      if test_op1 + test_op2 == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[b-df-hj-np-tv-z]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^0$|00$/ unless s =~ /[^01]/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if price <= 0 or isbn.length == 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    ("$#{@price.round(2)}".length - ("$#{@price.round(2)}" =~ /\./)) == 2 ? "$#{@price.round(2)}" + "0" : "$#{@price.round(2)}"
+  end
 end
