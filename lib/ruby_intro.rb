d152900089b218b317e8b035bb35423c8922f708
troy-fulton <troyfulton@tamu.edu>
2020-01-26 17:16:39 -0600

    Added comments

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -25,6 +25,8 @@ def sum_to_n? arr, n
   if arr.length <= 1
     return false
   end
+  # Not the most efficient method... but drop each element
+  # one at a time and try summing it with each of the others
   tmp_arr = arr
   for test_op1 in arr
     tmp_arr = tmp_arr.drop(1)
@@ -48,6 +50,8 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
+  # Has to be either a 0 or end with 00
+  # Gives nil (falsey) otherwise
   s =~ /^0$|00$/ unless s =~ /[^01]/
 end
 
@@ -65,6 +69,11 @@ class BookInStock
   attr_accessor :isbn, :price
   
   def price_as_string
+    # A little gross, but fits on one line!
+    # "$#{@price.round(2)}" gives what you want in the common case, but when
+    # there is no tens place in the decimal, a regex matcher will find the "."
+    # at the index two less than the size of that string, in which case, one 
+    # more 0 needs to be added
     ("$#{@price.round(2)}".length - ("$#{@price.round(2)}" =~ /\./)) == 2 ? "$#{@price.round(2)}" + "0" : "$#{@price.round(2)}"
   end
 end
